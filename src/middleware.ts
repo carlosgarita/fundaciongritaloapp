4bdf64be93f22d70f16272312baf577a6670fe1f
Carlos Garita <garitadaniel@hotmail.com>
2026-07-01 13:28:27 +0200

    PNGs de iconos con fondo transparente

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,6 @@ export default auth;
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon\\.(?:ico|png)|sw\\.js|manifest\\.json|robots\\.txt|sitemap\\.xml|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)",
   ],
 };
